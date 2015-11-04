5f969ce065ead0f7877d3c7a05afedd3660de56d
Anatoliy Goma <anatolij.goma@gmail.com>
2015-11-04 10:02:02 -0500

    Minor fix.

--- src/android/GPS.java
+++ src/android/GPS.java
@@ -31,169 +31,21 @@ public class GPS extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-//        try {
 
         if (action.equals("isGPSEnabled")) {
             actionIsEnableGPS(args, callbackContext);
             return true;
-        } else if (action.equals("gotoGPSSettings")) {
-            actionGotoGPSSettings(args, callbackContext);
+        } else if (action.equals("goToGPSSettings")) {
+            actionGoToGPSSettings(args, callbackContext);
             return true;
         } else if (action.equals("checkGPS")) {
             actionCheckGPS(args, callbackContext);
             return true;
         }
 
-
-//            if (action.equals("startActivity")) {
-//                if (args.length() != 1) {
-//                    //return new PluginResult(PluginResult.Status.INVALID_ACTION);
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//                    return false;
-//                }
-//
-//                // Parse the arguments
-//				final CordovaResourceApi resourceApi = webView.getResourceApi();
-//                JSONObject obj = args.getJSONObject(0);
-//                String type = obj.has("type") ? obj.getString("type") : null;
-//                Uri uri = obj.has("url") ? resourceApi.remapUri(Uri.parse(obj.getString("url"))) : null;
-//                JSONObject extras = obj.has("extras") ? obj.getJSONObject("extras") : null;
-//                Map<String, String> extrasMap = new HashMap<String, String>();
-//
-//                // Populate the extras if any exist
-//                if (extras != null) {
-//                    JSONArray extraNames = extras.names();
-//                    for (int i = 0; i < extraNames.length(); i++) {
-//                        String key = extraNames.getString(i);
-//                        String value = extras.getString(key);
-//                        extrasMap.put(key, value);
-//                    }
-//                }
-//
-//                startActivity(obj.getString("action"), uri, type, extrasMap);
-//                //return new PluginResult(PluginResult.Status.OK);
-//                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
-//                return true;
-//
-//            } else if (action.equals("hasExtra")) {
-//                if (args.length() != 1) {
-//                    //return new PluginResult(PluginResult.Status.INVALID_ACTION);
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//                    return false;
-//                }
-//                Intent i = ((CordovaActivity)this.cordova.getActivity()).getIntent();
-//                String extraName = args.getString(0);
-//                //return new PluginResult(PluginResult.Status.OK, i.hasExtra(extraName));
-//                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, i.hasExtra(extraName)));
-//                return true;
-//
-//            } else if (action.equals("getExtra")) {
-//                if (args.length() != 1) {
-//                    //return new PluginResult(PluginResult.Status.INVALID_ACTION);
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//                    return false;
-//                }
-//                Intent i = ((CordovaActivity)this.cordova.getActivity()).getIntent();
-//                String extraName = args.getString(0);
-//                if (i.hasExtra(extraName)) {
-//                    //return new PluginResult(PluginResult.Status.OK, i.getStringExtra(extraName));
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, i.getStringExtra(extraName)));
-//                    return true;
-//                } else {
-//                    //return new PluginResult(PluginResult.Status.ERROR);
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
-//                    return false;
-//                }
-//            } else if (action.equals("getUri")) {
-//                if (args.length() != 0) {
-//                    //return new PluginResult(PluginResult.Status.INVALID_ACTION);
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//                    return false;
-//                }
-//
-//                Intent i = ((CordovaActivity)this.cordova.getActivity()).getIntent();
-//                String uri = i.getDataString();
-//                //return new PluginResult(PluginResult.Status.OK, uri);
-//                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, uri));
-//                return true;
-//            } else if (action.equals("onNewIntent")) {
-//            	//save reference to the callback; will be called on "new intent" events
-//                this.onNewIntentCallbackContext = callbackContext;
-//
-//                if (args.length() != 0) {
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//                    return false;
-//                }
-//
-//                PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
-//                result.setKeepCallback(true); //re-use the callback on intent events
-//                callbackContext.sendPluginResult(result);
-//                return true;
-//                //return result;
-//            } else if (action.equals("sendBroadcast"))
-//            {
-//                if (args.length() != 1) {
-//                    //return new PluginResult(PluginResult.Status.INVALID_ACTION);
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//                    return false;
-//                }
-//
-//                // Parse the arguments
-//                JSONObject obj = args.getJSONObject(0);
-//
-//                JSONObject extras = obj.has("extras") ? obj.getJSONObject("extras") : null;
-//                Map<String, String> extrasMap = new HashMap<String, String>();
-//
-//                // Populate the extras if any exist
-//                if (extras != null) {
-//                    JSONArray extraNames = extras.names();
-//                    for (int i = 0; i < extraNames.length(); i++) {
-//                        String key = extraNames.getString(i);
-//                        String value = extras.getString(key);
-//                        extrasMap.put(key, value);
-//                    }
-//                }
-//
-//                sendBroadcast(obj.getString("action"), extrasMap);
-//                //return new PluginResult(PluginResult.Status.OK);
-//                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
-//                return true;
-//            }
-
-        //return new PluginResult(PluginResult.Status.INVALID_ACTION);
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
         return false;
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//            String errorMessage=e.getMessage();
-//            //return new PluginResult(PluginResult.Status.JSON_EXCEPTION);
-//            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION,errorMessage));
-//            return false;
-//        }
     }
-//
-//    public static void displayPromptForEnablingGPS(final Activity activity) {
-//
-//        final AlertDialog.Builder builder =  new AlertDialog.Builder(activity);
-//        final String action = Settings.ACTION_LOCATION_SOURCE_SETTINGS;
-//        final String message = "Do you want open GPS setting?";
-//
-//        builder.setMessage(message)
-//                .setPositiveButton("OK",
-//                        new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface d, int id) {
-//                                activity.startActivity(new Intent(action));
-//                                d.dismiss();
-//                            }
-//                        })
-//                .setNegativeButton("Cancel",
-//                        new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface d, int id) {
-//                                d.cancel();
-//                            }
-//                        });
-//        builder.create().show();
-//    }
 
     @Override
     public void onNewIntent(Intent intent) {
@@ -209,7 +61,7 @@ public class GPS extends CordovaPlugin {
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, isGPSEnabled()));
     }
 
-    private void actionGotoGPSSettings(JSONArray args, CallbackContext callbackContext) {
+    private void actionGoToGPSSettings(JSONArray args, CallbackContext callbackContext) {
         showAlertDialog();
     }
 
@@ -233,7 +85,7 @@ public class GPS extends CordovaPlugin {
         builder.setMessage("Please turn on GPS in High Accuracy mode in order to use application.");
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialogInterface, int i) {
-                gotoGPSSettings();
+                goToGPSSettings();
                 closeApplication();
             }
         });
@@ -242,7 +94,7 @@ public class GPS extends CordovaPlugin {
         alertDialog.show();
     }
 
-    private void gotoGPSSettings() {
+    private void goToGPSSettings() {
         cordovaActivity.startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0);
     }
 
